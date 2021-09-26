f5f3fe62916b27553cb0225def10f1bdeae2e21a
Frank <frank.lora.93@gmail.com>
2021-09-26 12:57:43 +0200

    Añadir loading a la app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="container">
-        <router-view>
-            <Home />
-        </router-view>
+        <LoadingComponent v-if="showLoading" />
+        <router-view> </router-view>
     </div>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
+import LoadingComponent from "./components/LoadingComponent.vue";
+import { mapState } from "vuex";
+
 export default {
     name: "App",
     components: {
-        Home,
+        LoadingComponent,
+    },
+    computed: {
+        ...mapState(["showLoading"]),
     },
 };
 </script>
@@ -22,6 +26,150 @@ body {
     background-color: #edf2f8;
     * {
         box-sizing: border-box;
+    }
+}
+.m {
+    &-0 {
+        margin: 0 !important;
+    }
+    &y {
+        &-0 {
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        &-1 {
+            margin-top: 40px;
+            margin-bottom: 40px;
+        }
+        &-2 {
+            margin-top: 32px;
+            margin-bottom: 32px;
+        }
+        &-3 {
+            margin-top: 24px;
+            margin-bottom: 24px;
+        }
+        &-4 {
+            margin-top: 16px;
+            margin-bottom: 16px;
+        }
+        &-5 {
+            margin-top: 8px;
+            margin-bottom: 8px;
+        }
+    }
+    &x {
+        &-0 {
+            margin-left: 0px;
+            margin-right: 0px;
+        }
+        &-1 {
+            margin-left: 40px;
+            margin-right: 40px;
+        }
+        &-2 {
+            margin-left: 32px;
+            margin-right: 32px;
+        }
+        &-3 {
+            margin-left: 24px;
+            margin-right: 24px;
+        }
+        &-4 {
+            margin-left: 16px;
+            margin-right: 16px;
+        }
+        &-5 {
+            margin-left: 8px;
+            margin-right: 8px;
+        }
+    }
+}
+.p {
+    &-0 {
+        padding: 0 !important;
+    }
+    &y {
+        &-0 {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+        &-1 {
+            padding-top: 40px;
+            padding-bottom: 40px;
+        }
+        &-2 {
+            padding-top: 32px;
+            padding-bottom: 32px;
+        }
+        &-3 {
+            padding-top: 24px;
+            padding-bottom: 24px;
+        }
+        &-4 {
+            padding-top: 16px;
+            padding-bottom: 16px;
+        }
+        &-5 {
+            padding-top: 8px;
+            padding-bottom: 8px;
+        }
+    }
+    &x {
+        &-0 {
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+        &-1 {
+            padding-left: 40px;
+            padding-right: 40px;
+        }
+        &-2 {
+            padding-left: 32px;
+            padding-right: 32px;
+        }
+        &-3 {
+            padding-left: 24px;
+            padding-right: 24px;
+        }
+        &-4 {
+            padding-left: 16px;
+            padding-right: 16px;
+        }
+        &-5 {
+            padding-left: 8px;
+            padding-right: 8px;
+        }
+    }
+}
+.d {
+    &-flex {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
+.justify {
+    &-start {
+        justify-content: flex-start;
+    }
+    &-center {
+        justify-content: center;
+    }
+    &-end {
+        justify-content: flex-end;
+    }
+}
+.align {
+    &-items {
+        &-start {
+            align-items: flex-start;
+        }
+        &-center {
+            align-items: center;
+        }
+        &-end {
+            align-items: flex-end;
+        }
     }
 }
 .container {
@@ -100,120 +248,6 @@ body {
                     width: 100%;
                     flex-basis: 100%;
                 }
-            }
-        }
-    }
-    .m {
-        &-0 {
-            margin: 0 !important;
-        }
-        &y {
-            &-0 {
-                margin-top: 0px;
-                margin-bottom: 0px;
-            }
-            &-1 {
-                margin-top: 40px;
-                margin-bottom: 40px;
-            }
-            &-2 {
-                margin-top: 32px;
-                margin-bottom: 32px;
-            }
-            &-3 {
-                margin-top: 24px;
-                margin-bottom: 24px;
-            }
-            &-4 {
-                margin-top: 16px;
-                margin-bottom: 16px;
-            }
-            &-5 {
-                margin-top: 8px;
-                margin-bottom: 8px;
-            }
-        }
-        &x {
-            &-0 {
-                margin-left: 0px;
-                margin-right: 0px;
-            }
-            &-1 {
-                margin-left: 40px;
-                margin-right: 40px;
-            }
-            &-2 {
-                margin-left: 32px;
-                margin-right: 32px;
-            }
-            &-3 {
-                margin-left: 24px;
-                margin-right: 24px;
-            }
-            &-4 {
-                margin-left: 16px;
-                margin-right: 16px;
-            }
-            &-5 {
-                margin-left: 8px;
-                margin-right: 8px;
-            }
-        }
-    }
-    .p {
-        &-0 {
-            padding: 0 !important;
-        }
-        &y {
-            &-0 {
-                padding-top: 0px;
-                padding-bottom: 0px;
-            }
-            &-1 {
-                padding-top: 40px;
-                padding-bottom: 40px;
-            }
-            &-2 {
-                padding-top: 32px;
-                padding-bottom: 32px;
-            }
-            &-3 {
-                padding-top: 24px;
-                padding-bottom: 24px;
-            }
-            &-4 {
-                padding-top: 16px;
-                padding-bottom: 16px;
-            }
-            &-5 {
-                padding-top: 8px;
-                padding-bottom: 8px;
-            }
-        }
-        &x {
-            &-0 {
-                padding-left: 0px;
-                padding-right: 0px;
-            }
-            &-1 {
-                padding-left: 40px;
-                padding-right: 40px;
-            }
-            &-2 {
-                padding-left: 32px;
-                padding-right: 32px;
-            }
-            &-3 {
-                padding-left: 24px;
-                padding-right: 24px;
-            }
-            &-4 {
-                padding-left: 16px;
-                padding-right: 16px;
-            }
-            &-5 {
-                padding-left: 8px;
-                padding-right: 8px;
             }
         }
     }
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+        showLoading: false,
+        urlApi: 'https://swapi.dev/api/',
+    },
+    mutations: {
+        setShowLoading(state, showLoading = false) {
+            state.showLoading = showLoading;
+        }
+    },
+    actions: {},
 });
